cb759be2a2c480dbb56987d2fd29384d2ade4878
Matt <mjd014@live.co.uk>
2023-09-02 21:40:48 +0000

    moved all created elements into addTask function as was not appending to each ul element.

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,16 +4,12 @@ document.addEventListener("DOMContentLoaded", function () {
     // Focus on input element on page load.
     input.focus();
 
-    // Important Task Element.
-    let priorityElement = document.createElement("li");
-    // Important Btn
-    let importantBtn = document.createElement("button");
-    importantBtn.innerHTML = '<i class="fa-solid fa-circle-exclamation fa-xl"></i>';
 
     let addTaskBtn = document.getElementById("addTaskBtn");
-    // Add task btn "click" event listener.
+    // Adding addTask function to btn.
     addTaskBtn.addEventListener("click", addTask);
 
+    // Task List Div.
     let taskListDiv = document.getElementById("taskListDiv");
 
     // Add task function.
@@ -22,10 +18,29 @@ document.addEventListener("DOMContentLoaded", function () {
         let taskNameElement = document.createElement("li");
         taskNameElement.innerText = input.value;
         taskNameElement.classList.add("text-truncate", "list-group-item", "w-25");
+
+        // Priority Element.
+        let priorityElement = document.createElement("li");
+        // Important Btn
+        let importantBtn = document.createElement("button");
+        importantBtn.innerHTML = '<i class="fa-solid fa-circle-exclamation fa-xl" style="color: #ff0000"></i>';
+        importantBtn.classList.add("btn", "p-0", "mx-lg-4", "mx-4");
+        // Up Btn.
+        let upBtn = document.createElement("button");
+        upBtn.innerHTML = '<i class="fa-regular fa-square-caret-up fa-xl" style="color: #388eff;"></i>';
+        // Down Btn.
+        let downBtn = document.createElement("button");
+        downBtn.innerHTML = '<i class="fa-regular fa-square-caret-down fa-xl" style="color: #388eff;"></i>';
+        [upBtn, importantBtn, downBtn].forEach((button) => {
+            button.classList.add("btn", "p-0");
+        });
+        priorityElement.append(upBtn, importantBtn, downBtn);
+        priorityElement.classList.add("list-group-item", "w-25", "px-0");
+
         // Create individual task row, containing features.
         let taskRowUl = document.createElement("ul");
         taskRowUl.classList.add("list-group", "list-group-horizontal", "mt-2");
-        taskRowUl.append(taskNameElement);
+        taskRowUl.append(taskNameElement, priorityElement);
         taskListDiv.append(taskRowUl);
         input.focus();
         input.value = "";
