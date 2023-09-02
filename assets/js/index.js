b4b0e6b6d45554deafb18a91c4bf620d6ff4549c
Matt <mjd014@live.co.uk>
2023-09-02 20:32:26 +0000

    moved taskNameElement into addTask function as it was replacing every li element when adding task.

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,28 +3,29 @@ document.addEventListener("DOMContentLoaded", function () {
     let input = document.getElementById("usersInput");
     // Focus on input element on page load.
     input.focus();
-    
-    // Task Name Element.
-    let taskNameElement = document.createElement("li");
-    taskNameElement.classList.add("text-truncate");
 
     // Important Task Element.
-    let importanceElement = document.createElement("li");
+    let priorityElement = document.createElement("li");
+    // Important Btn
     let importantBtn = document.createElement("button");
     importantBtn.innerHTML = '<i class="fa-solid fa-circle-exclamation fa-xl"></i>';
-    
+
     let addTaskBtn = document.getElementById("addTaskBtn");
     // Add task btn "click" event listener.
     addTaskBtn.addEventListener("click", addTask);
-    
+
     let taskListDiv = document.getElementById("taskListDiv");
 
     // Add task function.
-    function addTask () {
-        taskNameElement.innerHTML = input.value;
+    function addTask() {
+        // Task Name Element.
+        let taskNameElement = document.createElement("li");
+        taskNameElement.innerText = input.value;
+        taskNameElement.classList.add("text-truncate", "list-group-item", "w-25");
         // Create individual task row, containing features.
         let taskRowUl = document.createElement("ul");
-        taskRowUl.innerText = taskNameElement.innerHTML;
+        taskRowUl.classList.add("list-group", "list-group-horizontal", "mt-2");
+        taskRowUl.append(taskNameElement);
         taskListDiv.append(taskRowUl);
         input.focus();
         input.value = "";
