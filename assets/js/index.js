4d4d5619395341a584d20a67d739591af869534a
Matt <mjd014@live.co.uk>
2023-09-03 11:56:41 +0000

    created done, remove li elements and their respective button elements. Tidied up code.

--- assets/js/index.js
+++ assets/js/index.js
@@ -14,17 +14,24 @@ document.addEventListener("DOMContentLoaded", function () {
 
     // Add task function.
     function addTask() {
+
+        let [taskNameElement, priorityElement, doneElement, removeElement] = [
+            document.createElement("li"),
+            document.createElement("li"),
+            document.createElement("li"),
+            document.createElement("li")
+        ];
         // Task Name Element.
-        let taskNameElement = document.createElement("li");
+        // let taskNameElement = document.createElement("li");
         taskNameElement.innerText = input.value;
         taskNameElement.classList.add("text-truncate", "list-group-item", "w-25");
 
         // Priority Element.
-        let priorityElement = document.createElement("li");
+        // let priorityElement = document.createElement("li");
         // Important Btn
         let importantBtn = document.createElement("button");
-        importantBtn.innerHTML = '<i class="fa-solid fa-circle-exclamation fa-xl" style="color: #ff0000"></i>';
-        importantBtn.classList.add("btn", "p-0", "mx-lg-4", "mx-4");
+        importantBtn.innerHTML = '<i class="fa-solid fa-circle-exclamation fa-xl"></i>';
+        importantBtn.classList.add("mx-lg-4", "mx-4", "red-btn");
         // Up Btn.
         let upBtn = document.createElement("button");
         upBtn.innerHTML = '<i class="fa-regular fa-square-caret-up fa-xl" style="color: #388eff;"></i>';
@@ -37,10 +44,24 @@ document.addEventListener("DOMContentLoaded", function () {
         priorityElement.append(upBtn, importantBtn, downBtn);
         priorityElement.classList.add("list-group-item", "w-25", "px-0");
 
+        // Done & Remove btns.
+        let [doneBtn, removeBtn] = [
+            document.createElement("button"),
+            document.createElement("button"),
+        ];
+
+        doneBtn.classList.add("btn", "btn-sm", "btn-success", "w-75"); 
+        removeBtn.classList.add("btn", "btn-sm", "btn-danger", "w-75"); 
+        [doneBtn.innerText, removeBtn.innerText] = ["Done", "Remove"];
+        doneElement.append(doneBtn);
+        removeElement.append(removeBtn);
+        doneElement.classList.add("list-group-item", "w-25");
+        removeElement.classList.add("list-group-item", "w-25");
+
         // Create individual task row, containing features.
         let taskRowUl = document.createElement("ul");
         taskRowUl.classList.add("list-group", "list-group-horizontal", "mt-2");
-        taskRowUl.append(taskNameElement, priorityElement);
+        taskRowUl.append(taskNameElement, priorityElement, doneElement, removeElement);
         taskListDiv.append(taskRowUl);
         input.focus();
         input.value = "";
