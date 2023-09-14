a1e73a9c80acac5031c25ac02bcf1800439814e7
Matt <mjd014@live.co.uk>
2023-09-14 13:38:07 +0000

    modified and cleaned up code, made it more efficient.

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -1,19 +1,3 @@
-// Collecting all elements via class names.
-let infoContainers = document.getElementsByClassName("info-container");
-// Turning the html collections into an array individually.
-let containersArray = [...infoContainers];
-
-// Adding click event to each container.
-containersArray.forEach((container) => {
-    container.addEventListener("click", function () {
-        if (!container.children[1].classList.contains("d-none")) {
-            container.children[1].classList.add("d-none");
-        } else {
-            container.children[1].classList.replace("d-none", "down-fade");
-        };
-    });
-});
-
 let helpHeadings = document.querySelectorAll(".help-heading");
 let  = [...helpHeadings];
 
@@ -21,6 +5,8 @@ helpHeadings.forEach((heading) => {
     heading.addEventListener("click", function () {
         if (!this.firstElementChild.classList.contains("rotate-90")) {
             this.firstElementChild.classList.toggle("rotate-90");
+            this.nextElementSibling.classList.toggle("d-none");
+            this.nextElementSibling.classList.toggle("down-fade");
         };
         if (this.firstElementChild.classList.contains("reverse-90") === true) {
             this.firstElementChild.classList.toggle("reverse-90");
@@ -28,6 +14,8 @@ helpHeadings.forEach((heading) => {
         this.addEventListener("click", function () {
             this.firstElementChild.classList.toggle("reverse-90");
             this.firstElementChild.classList.toggle("rotate-90");
+            this.nextElementSibling.classList.toggle("d-none");
+            this.nextElementSibling.classList.toggle("down-fade");
         });
     });
 });
