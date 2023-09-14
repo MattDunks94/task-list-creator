88c34091c9bdc338f4e962ca91bd5dece7eb2bd6
Matt <mjd014@live.co.uk>
2023-09-14 10:01:52 +0000

    collected fa-chevron-right elements and made an array of them.

--- assets/js/animation.js
+++ assets/js/animation.js
@@ -1,8 +1,16 @@
+// Collecting all elements via class names.
 let infoContainers = document.getElementsByClassName("info-container");
+// Turning the html collections into an array individually.
 let containersArray = [...infoContainers];
 
+// Adding click event to each container.
 containersArray.forEach((container) => {
+    let chevrons = document.querySelectorAll(".fa-chevron-right");
+    let chevronsArray = [...chevrons];
     container.addEventListener("click", function () {
+        chevronsArray.forEach((chevron) => {
+            chevron.classList.add("rotate-90");
+        });
         if (!container.children[1].classList.contains("d-none")) {
             container.children[1].classList.add("d-none");
         } else {
