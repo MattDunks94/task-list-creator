49de42d008d03772a1da731b61d1da44b640f4b3
Matt <mjd014@live.co.uk>
2023-09-17 12:03:34 +0000

    added custom media queries for max screen size of 750px.

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -1,6 +1,6 @@
 let helpHeadings = document.querySelectorAll(".help-heading");
-let  = [...helpHeadings];
-
+let = [...helpHeadings];
+// Toggling animation classes for nav elements, icons.
 helpHeadings.forEach((heading) => {
     heading.addEventListener("click", function () {
         if (!this.firstElementChild.classList.contains("rotate-90")) {
@@ -19,3 +19,24 @@ helpHeadings.forEach((heading) => {
         });
     });
 });
+
+// CUSTOM MEDIA QUERIES.
+// Window event listener for resizing to width of < 750px. 
+window.addEventListener("resize", function () {
+    helpHeadings.forEach((heading) => {
+        if (this.window.innerWidth < 750) {
+            heading.parentElement.classList.replace("float-left", "m-auto");
+            heading.parentElement.classList.replace("col-3", "col-12");
+        } else {
+            heading.parentElement.classList.replace("m-auto", "float-left");
+            heading.parentElement.classList.replace("col-12", "col-3");
+        };
+    });
+});
+// Media query for fixed window max-width of 750px.
+if (window.matchMedia("(max-width: 750px)").matches) {
+    helpHeadings.forEach((heading) => {
+        heading.parentElement.classList.replace("float-left", "m-auto");
+        heading.parentElement.classList.replace("col-3", "col-12");
+    });
+};
