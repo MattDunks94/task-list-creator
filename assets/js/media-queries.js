855ffe6ec14cb50b10f2f3facfe5a96c7230b562
Matt <mjd014@live.co.uk>
2023-09-26 20:10:07 +0000

    created new resizing event listener for smaller screen sizes.

--- assets/js/media-queries.js
+++ assets/js/media-queries.js
@@ -1,9 +1,14 @@
 export function mediaQueries() {
     let taskListDiv = document.getElementById("taskListDiv");
     let tasks = taskListDiv.children;
+    // Collect all btns.
     let btns = document.querySelectorAll("#taskListDiv button");
     let btnsArray = [...btns];
-    // Media Queries for window resizing events.
+    let upBtn = btnsArray[0];
+    let downBtn = btnsArray[2];
+    let doneBtn = btnsArray[3];
+    let removeBtn = btnsArray[4];
+    // Media Query for window resizing below width of 540px.
     window.addEventListener("resize", function () {
         if (this.window.innerWidth < 540) {
             for (let task = 0; task < tasks.length; task++) {
@@ -19,9 +24,9 @@ export function mediaQueries() {
                 btn.classList.remove("btn-sm");
             });
             // upBtn
-            btnsArray[0].classList.add("float-left");
+            upBtn.classList.add("float-left");
             // downBtn
-            btnsArray[2].classList.add("float-right");
+            downBtn.classList.add("float-right");
         } else if (this.innerWidth >= 540) {
             for (let task = 0; task < tasks.length; task++) {
                 tasks[task].classList.add("list-group-horizontal");
@@ -36,12 +41,28 @@ export function mediaQueries() {
                 btn.classList.add("btn-sm");
             });
             // upBtn
-            btnsArray[0].classList.remove("float-left");
+            upBtn.classList.remove("float-left");
             // downBtn
-            btnsArray[2].classList.remove("float-right");
+            downBtn.classList.remove("float-right");
         };
     });
 
+    // Media Query for window resizing below width of 430px.
+    window.addEventListener("resize", function () {
+        if (this.innerWidth < 430) {
+            doneBtn.classList.replace("w-100", "w-48");
+            removeBtn.classList.replace("w-100", "w-48");
+            // Appending removeBtn to doneElement li element.
+            doneBtn.parentElement.append(removeBtn);
+            doneBtn.classList.add("float-left");
+            removeBtn.classList.add("float-right");
+            // Adding class 'd-none' to removeElement li element.
+            doneBtn.parentElement.nextElementSibling.classList.add("d-none");
+        };
+    });
+
+
+    // Window match width of 540px, for when not screen resizing.
     if (window.matchMedia("(max-width: 540px)").matches) {
         for (let task = 0; task < tasks.length; task++) {
             tasks[task].classList.remove("list-group-horizontal");
@@ -56,8 +77,8 @@ export function mediaQueries() {
             btn.classList.remove("btn-sm");
         });
         // upBtn
-        btnsArray[0].classList.add("float-left");
+        upBtn.classList.add("float-left");
         // downBtn
-        btnsArray[2].classList.add("float-right");
+        downBtn.classList.add("float-right");
     };
 };
