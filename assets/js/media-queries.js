b2e249c505b2ae890912acd6dce2cb91cf164a3e
Matt <mjd014@live.co.uk>
2023-09-27 07:33:39 +0000

    finished media queries for smaller screens, altered done, remove btns classLists and innerHTML on resize event and matchMedia.

--- assets/js/media-queries.js
+++ assets/js/media-queries.js
@@ -47,21 +47,6 @@ export function mediaQueries() {
         };
     });
 
-    // Media Query for window resizing below width of 430px.
-    window.addEventListener("resize", function () {
-        if (this.innerWidth < 430) {
-            doneBtn.classList.replace("w-100", "w-48");
-            removeBtn.classList.replace("w-100", "w-48");
-            // Appending removeBtn to doneElement li element.
-            doneBtn.parentElement.append(removeBtn);
-            doneBtn.classList.add("float-left");
-            removeBtn.classList.add("float-right");
-            // Adding class 'd-none' to removeElement li element.
-            doneBtn.parentElement.nextElementSibling.classList.add("d-none");
-        };
-    });
-
-
     // Window match width of 540px, for when not screen resizing.
     if (window.matchMedia("(max-width: 540px)").matches) {
         for (let task = 0; task < tasks.length; task++) {
@@ -80,5 +65,47 @@ export function mediaQueries() {
         upBtn.classList.add("float-left");
         // downBtn
         downBtn.classList.add("float-right");
+    };
+
+    // Media Query for window resizing below width of 430px.
+    window.addEventListener("resize", function () {
+        if (this.innerWidth < 430) {
+            doneBtn.classList.replace("w-100", "w-48");
+            removeBtn.classList.replace("w-100", "w-48");
+            // Change innerHTML to 'done' and 'remove' btns to FA icons.
+            doneBtn.innerHTML = '<i class="fa-regular fa-circle-check fa-xl" style="color: #ffffff;"></i>';
+            removeBtn.innerHTML = '<i class="fa-solid fa-trash-can fa-xl" style="color: #ffffff;"></i>';
+            // Appending removeBtn to doneElement li element.
+            doneBtn.parentElement.append(removeBtn);
+            doneBtn.classList.add("float-left");
+            removeBtn.classList.add("float-right");
+            // Temporarily adding class 'd-none' to removeElement li element.
+            doneBtn.parentElement.nextElementSibling.classList.add("d-none");
+        } else {
+            doneBtn.classList.replace("w-48", "w-100");
+            // Reinstating the original innerHTML to btns.
+            doneBtn.innerHTML = "Done";
+            removeBtn.innerHTML = "Remove";
+            removeBtn.classList.replace("w-48", "w-100");
+            doneBtn.classList.remove("float-left");
+            removeBtn.classList.remove("float-right");
+            // Re-appending removeBtn to original li element.
+            doneBtn.parentElement.nextElementSibling.append(btnsArray[4]);
+            // Removing class 'd-none' from removeElement li element.
+            doneBtn.parentElement.nextElementSibling.classList.remove("d-none");
+        };
+    });
+
+    if (window.matchMedia("(max-width: 430px)").matches) {
+        doneBtn.classList.replace("w-100", "w-48");
+        removeBtn.classList.replace("w-100", "w-48");
+        doneBtn.innerHTML = '<i class="fa-regular fa-circle-check fa-xl" style="color: #ffffff;"></i>';
+        removeBtn.innerHTML = '<i class="fa-solid fa-trash-can fa-xl" style="color: #ffffff;"></i>';
+        // Appending removeBtn to doneElement li element.
+        doneBtn.parentElement.append(removeBtn);
+        doneBtn.classList.add("float-left");
+        removeBtn.classList.add("float-right");
+        // Adding class 'd-none' to removeElement li element.
+        doneBtn.parentElement.nextElementSibling.classList.add("d-none");
     };
 };
