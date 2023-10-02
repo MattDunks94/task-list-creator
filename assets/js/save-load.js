f82bac38e6337e8314835d051e09ff09d9acb9a1
Matt <mjd014@live.co.uk>
2023-10-02 18:36:11 +0000

    add click event to loadBtn, renders saved localstorage data.

--- assets/js/save-load.js
+++ assets/js/save-load.js
@@ -1,10 +1,27 @@
-export { arrayOfTasks }
+export { savedTasks }
 
-let arrayOfTasks = localStorage.getItem("task-list") ? JSON.parse(localStorage.getItem("task-list")) : [];
+let savedTasks = localStorage.getItem("task-list") ? JSON.parse(localStorage.getItem("task-list")) : [];
 
 let saveBtn = document.getElementById("saveBtn");
 let loadBtn = document.getElementById("loadBtn");
+let taskListDiv = document.getElementById("taskListDiv");
+// Task Counter.
+let taskCounter = document.getElementById("taskCounter");
+// Task list table headers.
+let tableHeaders = document.getElementById("tableHeaders");
 
 saveBtn.addEventListener("click", function () {
-    localStorage.setItem("tasks", JSON.stringify(arrayOfTasks));
+    localStorage.setItem("tasks", JSON.stringify(savedTasks));
 });
+
+// loadBtn.addEventListener("click", function () {
+//     JSON.parse(localStorage.getItem("tasks")).forEach((task) => {
+//         taskListDiv.innerHTML = task;
+//         if (taskListDiv.children.length > 0) {
+//             taskCounter.classList.remove("d-none");
+//             taskCounter.firstElementChild.innerHTML = taskListDiv.children.length;
+//             tableHeaders.parentElement.classList.remove("d-none");
+//             document.getElementById("noTasks").classList.add("d-none");
+//         };
+//     });
+// });
