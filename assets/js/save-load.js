28e026002b3f441afb40135ac2ddc87b9e804dc4
Matt <mjd014@live.co.uk>
2023-10-07 18:51:58 +0000

    created function that loads local storage, added alert to saveBtn event, exported loadTaskList.
    Wrote new function 'loadTaskList' which loads saved localstorage and renders to the DOM, takes 3 parameters(input, func, alert), exported and imported to index.js, the parameters are variable input, addTask() function and alertBox for alert.

--- assets/js/save-load.js
+++ assets/js/save-load.js
@@ -1,43 +1,38 @@
-import { removeTask, moveTaskUp } from "../js/index.js";
-export { savedTasks }
+export {
+    savedTasks,
+    loadBtn,
+    loadTaskList
+}
+import {
+    savedTaskAlert,
+    loadedTaskAlert
+} from "../js/alerts.js";
 
 let savedTasks = localStorage.getItem("task-list") ? JSON.parse(localStorage.getItem("task-list")) : [];
 
 let saveBtn = document.getElementById("saveBtn");
 let loadBtn = document.getElementById("loadBtn");
-let taskListDiv = document.getElementById("taskListDiv");
-// Task Counter.
-let taskCounter = document.getElementById("taskCounter");
-// Task list table headers.
-let tableHeaders = document.getElementById("tableHeaders");
+let alertBox = document.getElementById("alertBox");
 
+// Saves task list to localstorage, displays alert.
 saveBtn.addEventListener("click", function () {
     localStorage.setItem("tasks", JSON.stringify(savedTasks));
+    alertBox.innerHTML = savedTaskAlert;
+    // Set 3s timeout for alert box display.
+    setTimeout(() => {
+        alertBox.innerHTML = "";
+    }, 3000);
 });
 
-// function loadTasks () {
-//     JSON.parse(localStorage.getItem("tasks")).forEach((task) => {
-//         taskListDiv.innerHTML = task;
-//     });
-// };
-
-loadBtn.addEventListener("click", function () {
+// Loads saved localstorage data, renders to the DOM via func() = addTask().
+function loadTaskList(input, func, alert) {
     JSON.parse(localStorage.getItem("tasks")).forEach((task) => {
-        taskListDiv.innerHTML = task;
-        if (taskListDiv.children.length > 0) {
-            taskCounter.classList.remove("d-none");
-            taskCounter.firstElementChild.innerHTML = taskListDiv.children.length;
-            tableHeaders.parentElement.classList.remove("d-none");
-            document.getElementById("noTasks").classList.add("d-none");
-        };
+        input.value = task;
+        func();
+        alert.innerHTML = loadedTaskAlert;
+        // Set 3s timeout for alert box display.
+        setTimeout(() => {
+            alertBox.innerHTML = "";
+        }, 3000);
     });
-    let taskBtns = document.querySelectorAll("#taskListDiv button");
-    for (let btn = 0; btn < taskBtns.length; btn++) {
-        // Remove Btn click event.
-        if (taskBtns[btn].classList.contains("btn-danger") === true) {
-            taskBtns[btn].addEventListener("click", function () {
-                taskBtns[btn].closest("ul").remove();
-            });
-        };
-    };
-});
+};
