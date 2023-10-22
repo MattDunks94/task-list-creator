75003d8e5f0cd735ef527b16056b085dd92f9f3a
Matt <mjd014@live.co.uk>
2023-10-22 20:41:25 +0000

    modified clearTaskList function.

--- assets/js/save-load.js
+++ assets/js/save-load.js
@@ -33,29 +33,29 @@ saveBtn.addEventListener("click", function () {
 });
 
 // Loads saved localstorage data, renders to the DOM via func() = addTask(), displays alert.
-function loadTaskList(input, func, alert) {
+function loadTaskList(input, callback, alert) {
     JSON.parse(localStorage.getItem("tasks")).forEach((task) => {
         input.value = task;
-        func();
+        callback();
         alert.innerHTML = loadedTaskAlert;
         // Set 3s timeout for alert box display.
         alertTimeout(alertBox);
     });
 };
 
-// Clears Task List.
-function clearTaskList(list, headers, counter, array1, array2) {
-    list.childNodes.forEach((ul) => {
-        ul.remove();
+/**  
+ * Clears Task List. 
+ * Resets arrayOfTasks and uniqueTasks arrays.
+ * Removes, displays elements to the DOM.
+*/
+function clearTaskList(headers, counter, array1, array2) {
+    let tasks = document.querySelectorAll("#taskListDiv ul");
+    tasks.forEach((task) => {
+        task.remove();
     });
     headers.parentElement.classList.add("d-none");
     document.getElementById("noTasks").classList.remove("d-none");
     counter.classList.add("d-none");
-    array1.forEach((task) => {
-        array1.pop(task);
-    });
-    array2.forEach((task) => {
-        array2.pop(task);
-    });
-    localStorage.clear();
+    array1.length = 0;
+    array2.length = 0;
 };
