c91b1755aac130860a91f1381847db879aa8be63
Matt <mjd014@live.co.uk>
2023-11-05 11:18:35 +0000

    added window resize event and window match for innerwidth < 350. Targeting saveLoadDropdown element.

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -1,5 +1,4 @@
 let helpHeadings = document.querySelectorAll(".help-heading");
-// let = [...helpHeadings];
 // Toggling animation classes for nav elements, icons.
 helpHeadings.forEach((heading) => {
     heading.addEventListener("click", function () {
@@ -44,5 +43,47 @@ if (window.matchMedia("(max-width: 750px)").matches) {
         heading.parentElement.classList.replace("col-3", "col-11");
         heading.nextElementSibling.classList.add("position-relative");
         heading.classList.add("mb-2");
+    });
+};
+
+// Save / Load / Clear Dropdown.
+let saveLoadDropdown = document.getElementById("saveLoadDropdown");
+let ul = saveLoadDropdown.firstElementChild;
+let liElements = ul.children;
+let clearBtn = document.getElementById("clearBtn");
+
+// Media Query for save/load/clear Dropdown. 
+window.addEventListener("resize", function () {
+    if (this.window.innerWidth < 350) {
+        // Turn dropdown into an horizontal list.
+        ul.classList.add("list-group-horizontal");
+        ul.classList.add("list-group");
+        [...liElements].forEach((element) => {
+            element.classList.add("list-group");
+            element.classList.add("mr-2");
+            element.firstElementChild.classList.replace("w-75", "w-100");
+            element.firstElementChild.classList.remove("mb-3");
+        });
+    } else if (this.window.innerWidth > 350) {
+        ul.classList.remove("list-group-horizontal");
+        ul.classList.remove("list-group");
+        [...liElements].forEach((element) => {
+            element.classList.remove("list-group");
+            element.classList.remove("mr-2");
+            element.firstElementChild.classList.replace("w-100", "w-75");
+            element.firstElementChild.classList.add("mb-3");
+        });
+        clearBtn.classList.remove("mb-3");
+    };
+});
+// Media query for fixed window max-width of 350px. 
+if (window.matchMedia("(max-width: 350px)").matches) {
+    ul.classList.add("list-group-horizontal");
+    ul.classList.add("list-group");
+    [...liElements].forEach((element) => {
+        element.classList.add("list-group");
+        element.classList.add("mr-2");
+        element.firstElementChild.classList.replace("w-75", "w-100");
+        element.firstElementChild.classList.remove("mb-3");
     });
 };
