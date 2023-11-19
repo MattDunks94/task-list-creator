98955753dd6d53b1e8d6b0e1e8f0bbcd449f07fa
Matt <mjd014@live.co.uk>
2023-11-19 12:36:01 +0000

    Modified accordionDropdown HTML content.
    replaced card-header button elements to a elements, added width classes.

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -6,9 +6,9 @@ let accordionDropdown = `
   <div class="card">
     <div class="card-header" id="headingOne">
       <h2 class="mb-0">
-        <button class="btn" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+        <a class="btn w-100" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
           Add Task
-        </button>
+        </a>
       </h2>
     </div>
 
@@ -23,9 +23,9 @@ let accordionDropdown = `
   <div class="card">
     <div class="card-header" id="headingTwo">
       <h2 class="mb-0">
-        <button class="btn collapsed" type="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+        <a class="btn collapsed w-100" type="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
           Remove Tasks
-        </button>
+        </a>
       </h2>
     </div>
     <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionDropdown">
@@ -38,9 +38,9 @@ let accordionDropdown = `
   <div class="card">
     <div class="card-header" id="headingThree">
       <h2 class="mb-0">
-        <button class="btn collapsed" type="button" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+        <a class="btn collapsed w-100" type="button" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
           Done Tasks
-        </button>
+        </a>
       </h2>
     </div>
     <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordionDropdown">
@@ -54,9 +54,9 @@ let accordionDropdown = `
   <div class="card">
     <div class="card-header" id="headingFour">
       <h2 class="mb-0">
-        <button class="btn collapsed" type="button" data-toggle="collapse" data-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
+        <a class="btn collapsed w-100" type="button" data-toggle="collapse" data-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
           Task Priority
-        </button>
+        </a>
       </h2>
     </div>
     <div id="collapseFour" class="collapse" aria-labelledby="headingFour" data-parent="#accordionDropdown">
